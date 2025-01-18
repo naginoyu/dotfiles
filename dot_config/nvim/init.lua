ae30b30e270b9c9dfbd86bfeb3c1f986c0c9cbb8
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-18 21:52:52 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -47,7 +47,7 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 
 -- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
+require("keymaps")
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
