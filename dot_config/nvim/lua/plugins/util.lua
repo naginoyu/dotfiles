bf5aa7d3170a8a0c877f08fcebe6840aaa2a03ec
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-19 21:44:52 +0900

    Update .config/nvim/lua/plugins/util.lua

--- dot_config/nvim/lua/plugins/util.lua
+++ dot_config/nvim/lua/plugins/util.lua
@@ -3,7 +3,14 @@ return {
 		"nvimdev/dashboard-nvim",
 		event = "VimEnter",
 		config = function()
-			require("dashboard").setup({})
+			require("dashboard").setup({
+				theme = "hyper",
+				config = {
+					week_header = {
+						enable = true,
+					},
+				},
+			})
 		end,
 		dependencies = { { "nvim-tree/nvim-web-devicons" } },
 	},
