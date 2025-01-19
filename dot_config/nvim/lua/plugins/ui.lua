b918e790f251c9cd5e77486cd7d0461ab5474021
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-19 10:39:40 +0900

    Update .config/nvim/lua/plugins/ui.lua

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,10 +1,9 @@
 return {
 	{
-		"akinsho/bufferline.nvim",
-		version = "*",
-		dependencies = "nvim-tree/nvim-web-devicons",
+		"romgrk/barbar.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("bufferline").setup({})
+			require("barbar").setup({})
 		end,
 	},
 
