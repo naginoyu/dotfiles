120f81bd4fc77bbd2af2372270517e39c3345cc5
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-20 11:47:43 +0900

    Update .config/nvim/lua/kickstart/plugins/neo-tree.lua

--- dot_config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ dot_config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -17,6 +17,25 @@ return {
 					["\\"] = "close_window",
 				},
 			},
+			follow_current_file = {
+				enabled = true, -- 現在のファイルを追跡する
+				leave_dirs_open = false, -- 自動展開されたディレクトリを閉じる
+			},
+			hijack_netrw_behavior = "open_default", -- netrw の代わりに NeoTree を使用
+			use_libuv_file_watcher = false, -- OS レベルのファイルウォッチャーを使用
+			cwd = function()
+				-- 現在のバッファのファイルパスを取得
+				local buf_path = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
+				-- ファイルパスからディレクトリを取得
+				local dir = vim.fn.fnamemodify(buf_path, ":h")
+				-- ディレクトリが存在する場合、それを cwd として設定
+				if vim.fn.isdirectory(dir) == 1 then
+					return dir
+				else
+					-- ディレクトリが存在しない場合、デフォルトの cwd を使用
+					return vim.fn.getcwd()
+				end
+			end,
 			filtered_items = {
 				visible = false,
 				hide_dotfiles = false,
