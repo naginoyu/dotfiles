dbbac03d5fbe195ee553c239df855697c0c50a6c
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-20 11:37:59 +0900

    Update .config/nvim/lua/keymaps.lua

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -25,7 +25,7 @@ function M.telescope()
 end
 
 function M.neotree()
-	vim.keymap.set("n", "<leader>ee", ":Neotree filesystem reveal left<CR>")
+	vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
 end
 
 function M.barbar()
