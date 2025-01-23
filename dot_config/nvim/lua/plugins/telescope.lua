7b6e79643ac3ba98d9768a4976bd38b1be0c2993
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-24 02:04:51 +0900

    Update .config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -13,5 +13,12 @@ return {
 		require("telescope").setup({})
 		require("telescope").load_extension("file_browser")
 		require("telescope").load_extension("fzf")
+
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+		vim.keymap.set("n", "<leader>fd", ":Telescope file_browser<CR>", { desc = "File browser" })
 	end,
 }
