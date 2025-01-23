1a2ae85f4795f6421ee6405bb10eda8ae3e7c46b
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-23 10:38:16 +0900

    Update .config/nvim/lua/kickstart/plugins/neo-tree.lua

--- dot_config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ dot_config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -11,7 +11,6 @@ return {
 		},
 		config = function()
 			require("neo-tree").setup({
-				close_if_last_window = true,
 				popup_border_style = "rounded",
 				enable_git_status = true,
 				enable_diagnostics = true,
