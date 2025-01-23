2560ae18227c1ce8c038ee29e4cf93614592e55b
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-24 01:53:32 +0900

    Update .config/nvim/lua/keymaps.lua

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -18,22 +18,6 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
 vim.keymap.set("n", "<C-q>", "<C-w>c", opts)
 
 -- Telescope
-function M.telescope()
-	local builtin = require("telescope.builtin")
-	vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-	vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-	vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
-	vim.keymap.set("n", "<leader>gl", builtin.git_commits, {})
-	vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-	vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
-	vim.keymap.set("n", "<Leader>fn", ":Telescope find_files cwd=~/.config/nvim<cr>", {})
-	vim.keymap.set("n", "<leader>fc", builtin.colorscheme, {})
-	vim.keymap.set("n", "<leader>fv", builtin.vim_options, {})
-	vim.keymap.set("n", "<leader>fk", builtin.keymaps, {})
-	vim.keymap.set("n", "<leader>fr", builtin.registers, {})
-	vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-	vim.keymap.set("n", "<leader>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
-end
 
 function M.neotree()
 	vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
