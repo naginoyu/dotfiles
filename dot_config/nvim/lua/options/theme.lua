54248e9954188992d722d8bc16b81e16d6602097
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-24 10:53:56 +0900

    Update .config/nvim/lua/options/theme.lua

--- dot_config/nvim/lua/options/theme.lua
+++ dot_config/nvim/lua/options/theme.lua
@@ -3,6 +3,9 @@ return {
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
-		opts = {},
+		config = function()
+			require("tokyonight").setup({})
+			vim.cmd([[colorscheme tokyonight]])
+		end,
 	},
 }
