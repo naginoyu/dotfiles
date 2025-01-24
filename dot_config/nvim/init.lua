530943a12790a8bcad3140e4bf066c98d2227bf2
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-24 10:48:19 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -646,6 +646,7 @@ require("lazy").setup({
 	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
 	--
 	require("options.theme"),
+  vim.cmd("colorscheme tokyonight"),
 
 	require("kickstart.plugins.indent_line"),
 	require("kickstart.plugins.lint"),
