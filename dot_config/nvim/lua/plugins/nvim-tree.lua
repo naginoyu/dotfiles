818d3ab56805901813b097c66fe67e2fe8979b1e
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-27 11:45:20 +0900

    Add .config/nvim/lua/plugins/nvim-tree.lua

---
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("nvim-tree").setup({
+				view = {
+					width = 100,
+					side = "left",
+				},
+				git = {
+					enable = true,
+				},
+				diagnostics = {
+					enable = true,
+				},
+				actions = {
+					open_file = {
+						quit_on_open = false,
+					},
+				},
+			})
+
+			vim.api.nvim_create_autocmd({ "VimEnter" }, {
+				callback = function()
+					require("nvim-tree.api").tree.open()
+				end,
+			})
+
+			vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+		end,
+	},
+}
