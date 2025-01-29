44eff065e2083e32e7158ac5e7d18201187e0715
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-29 10:59:14 +0900

    Update .config/nvim/lua/plugins/nvim-lspconfig.lua Update .config/nvim/lua/plugins/trouble.lua

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -13,8 +13,8 @@ return {
 				callback = function(args)
 					local client = vim.lsp.get_client_by_id(args.data.client_id)
 					if client then
-						vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = args.buf })
-						vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
+						vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { buffer = args.buf })
+						vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, { buffer = args.buf })
 						vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = args.buf })
 						vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = args.buf })
 					end
--- dot_config/nvim/lua/plugins/trouble.lua
+++ dot_config/nvim/lua/plugins/trouble.lua
@@ -1,18 +1,39 @@
 return {
 	{
 		"folke/trouble.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("trouble").setup({})
-
-			vim.api.nvim_create_autocmd("DiagnosticChanged", {
-				callback = function()
-					vim.schedule(function()
-						require("trouble").open()
-					end)
-				end,
-			})
-		end,
-		vim.keymap.set("n", "<leader>w", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true }),
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
 	},
 }
