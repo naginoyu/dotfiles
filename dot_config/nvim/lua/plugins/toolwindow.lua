a3f315a56881741e063d6697340ff6051b6cc01a
naginoyu <side.of.the.apple+dev@gmail.com>
2025-01-29 11:14:29 +0900

    Update .config/nvim/lua/plugins/toolwindow.lua

--- dot_config/nvim/lua/plugins/toolwindow.lua
+++ dot_config/nvim/lua/plugins/toolwindow.lua
@@ -2,7 +2,6 @@ return {
 	{
 		"EthanJWright/toolwindow.nvim",
 		config = function()
-			require("toolwindow").setup({})
 			vim.keymap.set("n", "<leader>tw", function()
 				require("toolwindow").toggle()
 			end, { noremap = true, silent = true, desc = "Toggle ToolWindow" })
