cac7b26f417d24d3206e5837a5cbec79e09a7166
naginoyu <side.of.the.apple+dev@gmail.com>
2025-02-04 00:32:25 +0900

    Update .config/nvim/lua/plugins/ddc.lua

--- dot_config/nvim/lua/plugins/ddc.lua
+++ dot_config/nvim/lua/plugins/ddc.lua
@@ -9,50 +9,55 @@ return {
 		"hrsh7th/vim-vsnip",
 	},
 	config = function()
+		-- LSPクライアントの設定を上書き
+		local lsp = require("lspconfig")
+
+		-- ddc用の専用capabilities作成
+		local ddc_capabilities = vim.lsp.protocol.make_client_capabilities()
+		ddc_capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+		-- tsserver設定の再定義
+		lsp.tsserver.setup({
+			capabilities = ddc_capabilities, -- cmp連携を排除
+			on_attach = function(client, bufnr)
+				client.server_capabilities.completionProvider = false -- LSP側の補完を無効化
+			end,
+		})
+
+		-- ddc-source-lspの優先度最大化設定
 		vim.fn["ddc#custom#patch_global"]({
 			ui = "native",
 			sources = { "lsp", "around" },
 			sourceOptions = {
-				["_"] = {
+				lsp = {
+					priority = 1000, -- 最高優先度
 					matchers = { "matcher_head" },
 					sorters = { "sorter_rank" },
-				},
-				lsp = {
-					snippetEngine = "vsnip",
 					keywordPattern = [[\%(\k\|[\.]\)\+]],
 					forceCompletionPattern = [[\.|:|"|'|`|$|\w+]],
+				},
+				around = { priority = 500 },
+			},
+			sourceParams = {
+				lsp = {
+					snippetEngine = "vsnip",
+					maxCandidateWidth = 80,
+					kindLabels = {
+						Class = " TS",
+						Function = " TS Function",
+						Interface = " TS Interface",
+					},
 				},
 			},
 		})
 
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "typescript", "typescriptreact" },
-			callback = function()
-				vim.fn["ddc#custom#set_buffer"]("sourceParams", {
-					lsp = {
-						maxCandidateWidth = 80,
-						enableResolveItem = true,
-						kindLabels = {
-							Class = " TS",
-							Function = " TS Function",
-							Interface = " TS Interface",
-							Enum = " TS Enum",
-							TypeParameter = " TS Generic",
-						},
-					},
-				})
+		-- キーマッピングの再設定
+		vim.keymap.set("i", "<Tab>", function()
+			return vim.fn.pumvisible() == 1 and "<Cmd>call pum#map#insert_relative(+1)<CR>" or "<Tab>"
+		end, { expr = true })
 
-				vim.fn["ddc#custom#set_buffer"]("autoCompleteEvents", {
-					"InsertEnter",
-					"TextChangedI",
-					"TextChangedP",
-					"TextChangedT",
-				})
-			end,
-		})
-
-		vim.keymap.set("i", "<Tab>", "<Cmd>call pum#map#insert_relative(+1)<CR>")
-		vim.keymap.set("i", "<S-Tab>", "<Cmd>call pum#map#insert_relative(-1)<CR>")
+		vim.keymap.set("i", "<S-Tab>", function()
+			return vim.fn.pumvisible() == 1 and "<Cmd>call pum#map#insert_relative(-1)<CR>" or "<S-Tab>"
+		end, { expr = true })
 	end,
 }
-
