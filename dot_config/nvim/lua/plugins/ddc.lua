8aa2a6730f7e9b72727c7c19f78edd6ebe93f7ed
naginoyu <side.of.the.apple+dev@gmail.com>
2025-02-03 11:25:47 +0900

    Update .config/nvim/lua/plugins/ddc.lua

--- dot_config/nvim/lua/plugins/ddc.lua
+++ dot_config/nvim/lua/plugins/ddc.lua
@@ -2,6 +2,14 @@ return {
 	"Shougo/ddc.vim",
 	dependencies = {
 		"vim-denops/denops.vim",
+		"Shougo/ddc-source-lsp",
 	},
-	config = function() end,
+	config = function()
+		vim.fn["ddc#custom#patch_global"]("sourceOptions", {
+			lsp = {
+				matchers = { "matcher_head" },
+				sorters = { "sorter_rank" },
+			},
+		})
+	end,
 }
