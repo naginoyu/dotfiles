cf7ace69155dbd375f66c541a029759448226473
naginoyu <side.of.the.apple+dev@gmail.com>
2025-02-04 00:51:17 +0900

    Update .config/nvim/lua/plugins/nvim-lspconfig.lua

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -22,4 +22,26 @@ return {
 			})
 		end,
 	},
+	{
+		"Shougo/ddc.vim",
+		dependencies = {
+			"Shougo/ddc-source-lsp",
+		},
+		config = function()
+			vim.cmd([[
+				call ddc#custom#patch_global('sources', ['lsp'])
+				call ddc#custom#patch_global('sourceOptions', {
+					\ 'lsp': {
+					\   'mark': 'lsp',
+					\   'matchers': ['matcher_head'],
+					\   'sorters': ['sorter_rank'],
+					\   'converters': ['converter_remove_overlap'],
+					\   'ignoreCase': v:true,
+					\   'forceCompletionPattern': '\.\w*|:\w*|->\w*',
+					\ },
+					\ })
+				call ddc#enable()
+			]])
+		end,
+	},
 }
